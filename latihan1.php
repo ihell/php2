fee059c74706e7bf7ad37520e35813b40b83b14d
ihell <ihell269@gmail.com>
2024-07-17 11:14:16 +0700

    cihuyy

---
+++ latihan1.php
@@ -0,0 +1,33 @@
+<!-- membuat table dengan perulangan -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan 3</title>
+</head>
+<body>
+    
+    <table border="1" cellpadding="10" cellspacing="0">
+      <?php
+        for( $i = 1; $i <= 3; $i++ ) {
+            echo "<tr>";
+            for( $j = 1; $j <= 5; $j++ ) {
+                echo "<td>$i,$j</td>";
+            }
+            echo "</tr>";
+        }
+      ?>
+
+    <?php for( $i = 1; $i <= 3; $i++ ) : ?>
+        <tr>
+            <?php for( $j = 1; $j <= 5; $j++ ) : ?>
+                <td><?= "$i, $j"; ?></td>
+            <?php endfor; ?>
+        </tr>
+    <?php endfor; ?>
+    </table>
+
+</body>
+</html>
